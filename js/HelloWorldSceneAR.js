a3207115fe69103fbba87f10cb41214165a29ef5
simistern <simon160@gmail.com>
2020-02-10 13:09:22 -0500

    First commit

---
+++ js/HelloWorldSceneAR.js
@@ -0,0 +1,90 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+import {StyleSheet} from 'react-native';
+
+import {
+  ViroARScene,
+  ViroText,
+  ViroMaterials,
+  ViroBox,
+  ViroConstants,
+} from 'react-viro';
+
+export default class HelloWorldSceneAR extends Component {
+
+  constructor() {
+    super();
+
+    // Set initial state here
+    this.state = {
+      text : "Initializing AR..."
+    };
+
+    // bind 'this' to functions
+    this._onInitialized = this._onInitialized.bind(this);
+  }
+
+  render() {
+    return (
+      <ViroARScene onTrackingUpdated={this._onInitialized} >
+        <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
+        <ViroBox
+            dragType="FixedToWorld"
+            onDrag={() => {}}
+            // materials={["box"]}
+            scale={[0.5, 0.5, 0.5]}
+            position={[1, -1, -1]}
+          />
+          <ViroBox
+            dragType="FixedToWorld"
+            onDrag={() => {}}
+            // materials={["box"]}
+            scale={[0.5, 0.5, 0.5]}
+            position={[0, -1, -1]}
+          />
+          <ViroBox
+            dragType="FixedToWorld"
+            onDrag={() => {}}
+            // materials={["box"]}
+            scale={[0.5, 0.5, 0.5]}
+            position={[-1, -1, -1]}
+          />
+      </ViroARScene>
+    );
+  }
+
+  _onInitialized(state, reason) {
+    if (state == ViroConstants.TRACKING_NORMAL) {
+      this.setState({
+        text : "Hello World!"
+      });
+    } else if (state == ViroConstants.TRACKING_NONE) {
+      // Handle loss of tracking
+    }
+  }
+}
+
+var styles = StyleSheet.create({
+  helloWorldTextStyle: {
+    fontFamily: 'Arial',
+    fontSize: 30,
+    color: '#ffffff',
+    textAlignVertical: 'center',
+    textAlign: 'center',  
+  },
+});
+
+ViroMaterials.createMaterials({
+  box: {
+    shininess: 5.0,
+    diffuseColor: "#ffffff",
+    lightingModel: "Blinn"
+  },
+  quad: {
+    diffuseColor: "rgba(0,0,0,0.2)"
+  }
+});
+
+module.exports = HelloWorldSceneAR;
