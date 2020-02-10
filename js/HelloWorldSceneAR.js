bd3b96d6bd0c7a6b955c858e12c3007c7684482d
simistern <simon160@gmail.com>
2020-02-10 15:05:33 -0500

    added 3d object with textures checkpoint

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -9,6 +9,7 @@ import {
   ViroText,
   ViroMaterials,
   ViroBox,
+  Viro3DObject,
   ViroConstants,
 } from 'react-viro';
 
@@ -19,7 +20,30 @@ export default class HelloWorldSceneAR extends Component {
 
     // Set initial state here
     this.state = {
-      text : "Initializing AR..."
+      text : "Initializing AR...", 
+      viroAssets: [{
+        id: "123",
+        type: "OBJ",
+        model: "https://s3.amazonaws.com/torontojs.arworkshop/SciFi_Fighter.obj",
+        resources: [
+          {
+            type: "roughnessTexture",
+            uri:
+              "https://s3.amazonaws.com/torontojs.arworkshop/SF_Fighter-Albedo.jpg"
+          },
+          {
+            type: "metalnessTexture",
+            uri:
+              "https://s3.amazonaws.com/torontojs.arworkshop/SF_Fighter-Albedo.jpg"
+          },
+          {
+            type: "diffuseTexture",
+            uri:
+              "https://s3.amazonaws.com/torontojs.arworkshop/SF_Fighter-Albedo.jpg"
+          }
+        ]
+      }
+    ]
     };
 
     // bind 'this' to functions
@@ -30,27 +54,28 @@ export default class HelloWorldSceneAR extends Component {
     return (
       <ViroARScene onTrackingUpdated={this._onInitialized} >
         <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
-        <ViroBox
-            dragType="FixedToWorld"
-            onDrag={() => {}}
-            // materials={["box"]}
-            scale={[0.5, 0.5, 0.5]}
-            position={[1, -1, -1]}
-          />
-          <ViroBox
-            dragType="FixedToWorld"
-            onDrag={() => {}}
-            // materials={["box"]}
-            scale={[0.5, 0.5, 0.5]}
-            position={[0, -1, -1]}
-          />
-          <ViroBox
-            dragType="FixedToWorld"
-            onDrag={() => {}}
-            // materials={["box"]}
-            scale={[0.5, 0.5, 0.5]}
-            position={[-1, -1, -1]}
-          />
+        <Viro3DObject
+          source={{
+            uri: 'https://s3.amazonaws.com/torontojs.arworkshop/SciFi_Fighter.obj'
+          }}
+          materials={["spaceship"]}
+          // ref={ar3dModelRef}
+          onLoadEnd={data => {
+            alert("Model Loaded");
+          }}
+          onError={event => {
+            alert("Error: ", event);
+          }}
+          onPress={() => alert("you touched!")}
+          onDrag={() => {}}
+          // onPinch={onPinch}
+          scale={[0.5, 0.5, 0.5]}
+          // onRotate={onRotate}
+          position={[0, 0, -10]}
+          // rotation={rotation}
+          type={this.state.viroAssets[0].type}
+          castsShadow={true}
+        />
       </ViroARScene>
     );
   }
@@ -60,7 +85,7 @@ export default class HelloWorldSceneAR extends Component {
       this.setState({
         text : "Hello World!"
       });
-    } else if (state == ViroConstants.TRACKING_NONE) {
+    } else if (state == ViroConstants.TRACKING_NONE) {      
       // Handle loss of tracking
     }
   }
@@ -81,6 +106,11 @@ ViroMaterials.createMaterials({
     shininess: 5.0,
     diffuseColor: "#ffffff",
     lightingModel: "Blinn"
+  },
+  spaceship: {
+    shininess: 5.0,
+    diffuseColor: "#ffffff",
+    diffuseTexture: require('../assets/SF_Fighter-Albedo.jpg')
   },
   quad: {
     diffuseColor: "rgba(0,0,0,0.2)"
